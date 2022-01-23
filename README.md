d2288c718261157c247979b4e946c3f8113fbace
Gerald Landl <glandl@duality.at>
2022-01-23 20:15:31 +0100

    lesson 5 done

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # vue3-course
 Project files for youtube video course https://www.youtube.com/watch?v=YrxBCBibVo0&amp;list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1
-# Lesson 5
-This is the starting point for lesson #5 https://youtu.be/KM1U6DqZf8M
+# Lesson 6
+This is the starting point for lesson #6 https://youtu.be/bc6czIBLKTg
--- modal-project/src/App.vue
+++ modal-project/src/App.vue
@@ -1,6 +1,26 @@
 <template>
   <h1>{{ title }}</h1>
-  <Modal />
+  <p>Wellcome ...</p>
+  
+  <teleport to=".modals" v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign up now</a>
+        <a href="#">more info</a>
+      </template>
+      <h1> {{ header }} </h1>
+      <p>{{ text }}</p>
+    </Modal>
+  </teleport>
+
+  <teleport to=".modals" v-if="showModal2">
+    <Modal theme="sale" @close="toggleModalTwo">
+      <h1>Sign up to the newsletter</h1>
+      <p>For updates and promo codes!</p>
+    </Modal>
+  </teleport>
+  <button @click="toggleModal">open modal (alt)</button>
+  <button @click="toggleModalTwo">open modal</button>
 </template>
 
 <script>
@@ -13,14 +33,26 @@ export default {
   },
   data(){
     return {
-      title: "My First Vue App :)"
+      title: "My First Vue App :)",
+      header: "Sign up for a givaway!",
+      text: "Grab your ninja swag for half price!",
+      showModal: false,
+      showModal2: false,
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModal2 = !this.showModal2
     }
   }
 }
 </script>
 
 <style>
-#app {
+#app, .modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
