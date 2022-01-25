3585c60f8010e8ffd5d6f73f0a839b16bc2573a3
Gerald Landl <glandl@duality.at>
2022-01-25 23:11:06 +0100

    lesson 10 done

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # vue3-course
 Project files for youtube video course https://www.youtube.com/playlist?list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1
-# Lesson 10
-This is the starting point for lesson #10 https://youtu.be/V-kxBWcPJfo
+# Lesson 11
+This is the starting point for lesson #11 https://youtu.be/0FwBjPeLqQ8
---
+++ dojo-blog/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+  </div>
+</template>
+
+<script>
+import { ref } from '@vue/reactivity'
+
+export default {
+  name: 'Home',
+  setup() {
+    const posts = ref([
+      { title: 'welcome to the blog', body: 'Lorem ipsum', id: 1},
+      { title: 'top 5 CSS tips', body: 'Lorem ipsum', id: 2},
+    ])
+
+    return { posts }
+  }
+}
+</script>
