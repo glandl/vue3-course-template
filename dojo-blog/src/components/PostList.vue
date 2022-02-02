8a012edea23207ae535ea8c33c29722544d7fcda
Gerald Landl <glandl@duality.at>
2022-02-02 22:47:25 +0100

    lesson 11 finished

--- dojo-blog/src/components/PostList.vue
+++ dojo-blog/src/components/PostList.vue
@@ -7,16 +7,17 @@
 </template>
 
 <script>
-import { onMounted } from '@vue/runtime-core'
+import { onMounted, onUnmounted, onUpdated } from '@vue/runtime-core'
 import SinglePost from './SinglePost.vue'
 
 export default {
     props: ['posts'],
     components: { SinglePost },
     setup(props) {
-        onMounted(() => {
-            
-        })
+        console.log(props.posts)
+        onMounted(() => console.log('component mounted'))
+        onUnmounted(() => console.log('component unmounted'))
+        onUpdated(() => console.log('component updated'))
     }
 
 }
--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -1,25 +1,30 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList :post="posts" />
-    <button>toggle posts</button>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList v-if="showPosts" :posts="posts" />
+    </div>
+    <div v-else>Loading...</div>
+    <button @click="showPosts = !showPosts">toggle posts</button>
+    <button @click="posts.pop()">delete a post</button>
   </div>
 </template>
 
 <script>
-import PostList from '../components/PostList.vue'
 import { ref } from '@vue/reactivity'
+import PostList from '../components/PostList.vue'
+import getPosts from '../composables/getPosts'
 
 export default {
   name: 'Home',
   components: { PostList },
   setup() {
-    const posts = ref([
-      { title: 'welcome to the blog', body: 'Lorem ipsum', id: 1},
-      { title: 'top 5 CSS tips', body: 'Lorem ipsum', id: 2},
-    ])
+    const { posts, error, load } = getPosts()
     const showPosts = ref(true)
-    return { posts, showPosts }
-  }
+    load()
+
+    return { posts, showPosts, error }
+  },
 }
 </script>
